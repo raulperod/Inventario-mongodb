c591a735d7d38f2602cbe5696067f41663a0eadd
Raul Perez <raulperod@gmail.com>
2017-03-05 13:47:29 -0700

    cambios en los productos

--- app_server/routes/routes_product.js
+++ app_server/routes/routes_product.js
@@ -6,10 +6,9 @@ var Consumo = require("../models/consumo").Consumo;
 var RegistroDeMovimiento = require("../models/registroDeMovimiento").RegistroDeMovimiento;
 var Baja = require("../models/baja").Baja;
 var router = express.Router();
-
+// gelishtime/products
 router.get("/",function(req,res){
-    // busca todos los productos de la base de datos donde
-    // permisos es igual a "Normal"
+    // busca todos los productos de la base de datos
     Producto.find({})
             .populate("categoria")
             .exec(function(err,productos){
@@ -18,78 +17,126 @@ router.get("/",function(req,res){
         res.render("./products/manager",{productos:productos});
       }else{ // si hubo error
         if(err) console.log(err); // imprimo el error
-        res.redirect("/almacen"); // redirecciono al inicio
+        res.redirect("/almacen"); // redirecciono al almacen
       }
     });
 });
-// para registrar un producto
+// gelishtime/products/new
 router.route("/new")
+      // Metodo GET
       .get(function(req,res){
-          Categoria.find({},function(err,categorias){
-            if(!err){
-              res.render("./products/new",{categorias:categorias});
-            }else{
-              console.log(err);
-              res.redirect("/products");
-            }
-          });
-      })
-      .post(function(req,res){
-        // crea un producto
-        // validar que el Producto no este repetido
-        Producto.findOne({nombre:req.body.nombre},function(err,producto){
-          // si no hay error y no hay producto repetido, entonces lo crea
-          if(!err && !producto){
-            // busca la sucursal para
-            Categoria.findOne({nombre:req.body.categoria},function(err,categoria){
-              // crea un producto nuevo con sus respectivos atributos
-              var producto = new Producto({
-                nombre: req.body.nombre,
-                codigo: req.body.codigo,
-                descripcion: req.body.descripcion,
-                minimo: parseInt(req.body.minimo),
-                categoria: categoria._id
-              });
-              // guarda al producto en la base de datos
-              producto.save().then(function(us){
-                res.redirect("/products");
-              }, function(err){ // si ocurre un error lo imprime
-                console.log(err);
-              });
-            });
-          }else{
-            console.log(err);
-            res.redirect("/products");
-          }
-        });
-      });
-// para editar un usuario
-router.route("/:idProducto")
-      .get(function(req,res){
-        Categoria.find({},function(err,categorias){
-          if(!err){
-            // busco al usuario
-            Producto.findById(req.params.idProducto,function(err,producto){
-              if(!err){
-                res.render("./products/update",{categorias:categorias,productoUpdate:producto});
-              }else{
-                // imprimo el error y lo redirecciono al administrador de productos
+          // busca todas las categorias
+          Categoria.find({}).exec(function(err,categorias){
+            if(!err && categorias){ // si no hay error y hay categorias
+              res.render("./products/new",{categorias:categorias,AlertNombre:false,AlertCodigo:false});
+            }else{ // si hubo un error
+              if(!categorias){ // si no hay categorias
+                res.redirect("/categories/new"); // se redirecciona a crear una
+              }else{ // si hubo un error
                 console.log(err);
                 res.redirect("/products");
               }
+            }
+          });
+      })
+      // Metodo POST
+      .post(function(req,res){
+        // crea un producto
+        // validar que existe un prodcuto con el mismo nombre o el mismo codigo
+        Producto.findOne({ $or: [ {nombre:req.body.nombre} , {codigo:req.body.codigo} ] } ).exec(function(err,producto){
+          // si no hay error y no hay producto repetido, entonces lo crea
+          if(!err && !producto){
+            // busca la sucursal para
+            Categoria.findOne({nombre:req.body.categoria}).exec(function(err,categoria){
+              if(!err && categoria){
+                // crea un producto nuevo con sus respectivos atributos
+                var producto = new Producto({
+                  nombre: req.body.nombre,
+                  codigo: req.body.codigo,
+                  descripcion: req.body.descripcion,
+                  minimo: parseInt(req.body.minimo),
+                  categoria: categoria._id
+                });
+                // guarda al producto en la base de datos
+                producto.save().then(function(us){
+                  res.redirect("/products");
+                }, function(err){ // si ocurre un error lo imprime
+                  console.log(err);
+                });
+              }else{ // si hubo un error
+                if(!categoria){ // si no existe la categoria
+                  res.redirect("/categories/new");
+                }else{ // si hubo un error
+                  console.log(err);
+                  res.redirect("/products");
+                }
+              }
             });
           }else{
-            console.log(err);
-            res.redirect("/products");
+            if(producto){ // si el producto ya existe
+              // busca todas las categorias
+              Categoria.find({}).exec(function(err,categorias){
+                if(!err && categorias){ // si no hay error y hay categorias
+                  if(producto.nombre == req.body.nombre){ // si lo que se repitio fue el nombre
+                    res.render("./products/new",{categorias:categorias,AlertNombre:true,AlertCodigo:false,nombre:req.body.nombre,codigo:req.body.codigo,descripcion:req.body.descripcion,minimo:req.body.minimo});
+                  }else{// si lo que se repitio fue el codigo
+                    res.render("./products/new",{categorias:categorias,AlertNombre:false,AlertCodigo:true,nombre:req.body.nombre,codigo:req.body.codigo,descripcion:req.body.descripcion,minimo:req.body.minimo});
+                  }
+                }else{ // si hubo un error
+                  if(!categorias){ // si no hay categorias
+                    res.redirect("/categories/new"); // se redirecciona a crear una
+                  }else{ // si hubo un error
+                    console.log(err);
+                    res.redirect("/products");
+                  }
+                }
+              });
+            }else{ // si hubo un error
+              console.log(err);
+              res.redirect("/products");
+            }
+          }
+        });
+      });
+// gelishtime/products/:idProducto
+router.route("/:idProducto")
+      // Metodo GET
+      .get(function(req,res){
+        // busco todas las categorias
+        Categoria.find({}).exec(function(err,categorias){
+          if(!err && categorias){ // si no hay error y hay categorias
+            // busco al producto
+            Producto.findById(req.params.idProducto).exec(function(err,producto){
+              if(!err && producto){ // si no hay error y el producto existe
+                res.render("./products/update",{categorias:categorias,productoUpdate:producto});
+              }else{ // si hubo un error
+                if(!producto){ // si no hay producto
+                  res.redirect("/products/new");
+                }else{ // si hubo un error
+                  console.log(err);
+                  res.redirect("/products");
+                }
+              }
+            });
+          }else{ // si hubo un error
+            if(!categorias){ // si no hay categorias
+              res.redirect("/categories/new");
+            }else{ // si hubo un error
+              console.log(err);
+              res.redirect("/products");
+            }
           }
         });
       })
+      // Metodo PUT
       .put(function(req,res){
+        // busco la categoria
         Categoria.findOne({nombre:req.body.categoria},function(err,categoria){
-          if(!err){
+          if(!err && categoria){ // si no hubo error y la categoria existe
             // busco el producto
             Producto.findById(req.params.idProducto,function(err,producto){
-              if(!err && producto){
+              if(!err && producto){ // si no hubo error y el producto existe
+                // edito el producto
                 res.locals.productoUpdate = producto;
                 res.locals.productoUpdate.nombre = req.body.nombre;
                 res.locals.productoUpdate.codigo = req.body.codigo;
@@ -100,33 +147,45 @@ router.route("/:idProducto")
                   if(err) console.log(err);
                   res.redirect("/products");
                 });
-              }else{
-                console.log(err);
-                res.redirect("/products");
+              }else{ // si hubo un error
+                if(!producto){ // si el producto no existe
+                  res.redirect("/products/new");
+                }else{ // si hubo un error
+                  console.log(err);
+                  res.redirect("/products");
+                }
               }
             });
-          }else{
-            console.log(err);
-            res.redirect("/products");
+          }else{ // si hubo un error
+            if(!categoria){ // si la categoria no existe
+              res.redirect("/categories/new");
+            }else{ // si hubo un error
+              console.log(err);
+              res.redirect("/products");
+            }
           }
         });
       })
-router.route("/:idProducto/delete")
+      // Metodo DELETE
       .delete(function(req,res){
-
-        Baja.remove({producto:req.params.idProducto},function(err){
+        // borra las bajas produccidas por el producto
+        Baja.remove({producto:req.params.idProducto}).exec(function(err){
           if(err) console.log(err);
         });
-        RegistroDeMovimiento.remove({producto:req.params.idProducto},function(err){
+        // borra los movimientos produccidos por el producto
+        RegistroDeMovimiento.remove({producto:req.params.idProducto}).exec(function(err){
           if(err) console.log(err);
         });
-        Consumo.remove({producto:req.params.idProducto},function(err){
+        // borra los productos en consumo del producto
+        Consumo.remove({producto:req.params.idProducto}).exec(function(err){
           if(err) console.log(err);
         });
-        Almacen.remove({producto:req.params.idProducto},function(err){
+        // borra los productos en el almacen del producto
+        Almacen.remove({producto:req.params.idProducto}).exec(function(err){
           if(err) console.log(err);
         });
-        Producto.findOneAndRemove({_id: req.params.idProducto},function(err){
+        // borra el producto
+        Producto.findOneAndRemove({_id: req.params.idProducto}).exec(function(err){
             if(err) console.log(err);
             res.redirect("/products");
         });
