a8ad4337858a8f836c8e1db27b643fcb646da0c4
Raul Perez <raulperod@gmail.com>
2017-03-06 18:48:07 -0700

    comentarios en routes_consumo

--- app_server/routes/routes_consumo.js
+++ app_server/routes/routes_consumo.js
@@ -5,51 +5,50 @@ var Producto = require("../models/producto").Producto;
 var Sucursal = require("../models/sucursal").Sucursal;
 var Baja = require("../models/baja").Baja;
 var router = express.Router();
-// mostrar lista de productos de la sucursal del usuario
+// gelishtime/consumos
 router.get("/",function(req,res){
-  // busca al usuario logeado
-  if(res.locals.usuario.permisos == 2){
-    // busca los pruductos del almacen donde sean de la sucursal del usuario
+  if(res.locals.usuario.permisos == 2){ // si es administrador general
+    // busca los productos en consumo de todas las sucursales
     Consumo.find({ })
           .populate("producto sucursal")
           .exec(function(err,consumos){
 
-      if(!err){
+      if(!err && consumo){ // si no hay error
         // le mandas los productos del almacen
         res.render("./consumos/manager",{consumos:consumos});
-      }else{
+      }else{ // si hubo error
         console.log(err);
         res.redirect("/almacen");
       }
     });
-  }else{
-    // busca los pruductos del almacen donde sean de la sucursal del usuario
+  }else{ // si es administrador de sucursal o recepcionista
+    // busca los pruductos en consumo donde sean de la sucursal del usuario
     Consumo.find({ sucursal:res.locals.usuario.sucursal })
             .populate("producto sucursal")
             .exec(function(err,consumos){
 
-      if(!err){
+      if(!err && consumos){ // si no hubo error
         // le mandas los productos del almacen
         res.render("./consumos/manager",{consumos:consumos});
-      }else{
+      }else{ // si hubo error
         console.log(err);
         res.redirect("/almacen");
       }
     });
   }
 });
-
-// para editar un producto en el almacen
+// gelishtime/consumos/:idConsumo
 router.route("/:idConsumo")
       .put(function(req,res){
-        // si no mandaron cambios
+        // si no mandaron cambios, redirecciona a consumo
         if(parseInt(req.body.cantidad) == 0) res.redirect("/consumos");
+        // busca el producto que cambiaron
         Consumo.findById(req.params.idConsumo,function(err,productoCon){
-          if(!err && productoCon){
+          if(!err && productoCon){ // si no hay error y el producto existe
             res.locals.productoConUpdate = productoCon;
             // si el numero que pusieron es mayor que el que tenian, entonces quedan 0 productos
             if( parseInt(req.body.cantidad) > res.locals.productoConUpdate.cantidad ){
-              // genera el registro
+              // genera la baja
               var baja = new Baja({
                 sucursal:res.locals.usuario.sucursal,
                 usuario:req.session.user_id,
@@ -59,7 +58,6 @@ router.route("/:idConsumo")
               res.locals.productoConUpdate.cantidad = 0;
               // guarda al producto en la base de datos
             }else{ // si no, solamente se resta la cantidad que mando
-
               // genera el registro
               var baja = new Baja({
                 sucursal:res.locals.usuario.sucursal,
@@ -67,19 +65,20 @@ router.route("/:idConsumo")
                 cantidad:parseInt(req.body.cantidad),
                 producto:productoCon.producto
               });
+              // se le resta la cantidad
               res.locals.productoConUpdate.cantidad -= parseInt(req.body.cantidad);
             }
-            // guarda al producto en la base de datos
+            // actualiza el producto en consumo
             res.locals.productoConUpdate.save(function(err){
               if(err) console.log(err);
             });
-            // guarda el registro
+            // guarda la baja
             baja.save().then(function(us){
               res.redirect("/consumos");
             }, function(err){ // si ocurre un error lo imprime
               console.log(err);
             });
-          }else{
+          }else{ // si hubo un error
             console.log(err);
             res.redirect("/consumos");
           }
