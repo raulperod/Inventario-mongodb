d92adcbded09e3d56d13eacf6dea4c1a33525e48
Raul Perez <raulperod@gmail.com>
2017-03-12 12:35:49 -0700

    error en el if :(

--- app_server/routes/routes_basico.js
+++ app_server/routes/routes_basico.js
@@ -12,7 +12,7 @@ router.route("/")
       .get(function(req,res){
         // busco las tecnicas de la sucursal del usuario
         Tecnica.find({sucursal:res.locals.usuario.sucursal},{nombre:1}).exec(function(err,tecnicas){
-          if(!err & tecnicas){ // si no hubo error y existen tecnicas
+          if(!err && tecnicas){ // si no hubo error y existen tecnicas
             // busca los productos basicos de la sucursal
             Producto.find({esBasico:true},{nombre:1}).exec(function(err,productos){
               if(!err && productos){ // si no hubo error y existen productos
