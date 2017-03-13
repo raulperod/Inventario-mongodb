a765459dfaa9685dbf29339c8a7a8658efe36ddf
Raul Perez <raulperod@gmail.com>
2017-03-12 20:34:18 -0700

    probando otra vez

--- app_server/routes/routes_product.js
+++ app_server/routes/routes_product.js
@@ -107,6 +107,15 @@ router.route("/new")
           }
         });
       });
+// gelishtime/products/table
+router.route("/table")
+      // Metodo GET
+      .get(function(req,res){
+        res.render("./products/table");
+      })
+      // Metodo POST
+      .post(function(req,res){
+});
 // gelishtime/products/:idProducto
 router.route("/:idProducto")
       // Metodo GET
@@ -323,6 +332,7 @@ router.route("/:idProducto")
             res.redirect("/products");
         });
   });
+/*
 // gelishtime/products/new-excel
 router.route("/new/excel")
       // Metodo GET
@@ -331,41 +341,6 @@ router.route("/new/excel")
       })
       // Metodo POST
       .post(function(req,res){
-        var exceltojson;
-        upload(req,res,function(err){
-            if(err){
-                 res.json({error_code:1,err_desc:err});
-                 return;
-            }
-            /** Multer gives us file info in req.file object */
-            if(!req.file){
-                res.json({error_code:1,err_desc:"No file passed"});
-                return;
-            }
-            /** Check the extension of the incoming file and
-             *  use the appropriate module
-             */
-            if(req.file.originalname.split('.')[req.file.originalname.split('.').length-1] === 'xlsx'){
-                exceltojson = xlsxtojson;
-            } else {
-                exceltojson = xlstojson;
-            }
-            console.log(req.file.path);
-            try {
-                exceltojson({
-                    input: req.file.path,
-                    output: null, //since we don't need output.json
-                    lowerCaseHeaders:true
-                }, function(err,result){
-                    if(err) {
-                        return res.json({error_code:1,err_desc:err, data: null});
-                    }
-                    res.json({error_code:0,err_desc:null, data: result});
-                });
-            } catch (e){
-                res.json({error_code:1,err_desc:"Corupted excel file"});
-            }
-        })
 
         var exceltojson;
         upload(req,res,function(err){
@@ -481,6 +456,6 @@ router.route("/new/excel")
         });
 
       });
-
+*/
 
 module.exports = router;
