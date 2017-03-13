22403d4ffd7938c55f2fc9d6c1616761832663d4
Raul Perez <raulperod@gmail.com>
2017-03-12 17:33:47 -0700

    probando con exel

--- app_server/routes/routes_product.js
+++ app_server/routes/routes_product.js
@@ -20,7 +20,15 @@ var storage = multer.diskStorage({
                       cb(null,file.fieldname+'-'+datetimestamp+'.'+file.originalname.split('.')[file.originalname.split('.').length-1])
           }
 });
-var upload = multer({storage:storage}).single('file');
+var upload = multer({ //multer settings
+    storage: storage,
+    fileFilter : function(req, file, callback) { //file filter
+        if (['xls', 'xlsx'].indexOf(file.originalname.split('.')[file.originalname.split('.').length-1]) === -1) {
+            return callback(new Error('Wrong extension type'));
+        }
+        callback(null, true);
+    }
+}).single('file');
 // ------------
 // gelishtime/products
 router.get("/",function(req,res){
@@ -349,11 +357,50 @@ router.route("/new/excel")
       .post(function(req,res){
         var exceltojson;
         upload(req,res,function(err){
+            if(err){
+                 res.json({error_code:1,err_desc:err});
+                 return;
+            }
+            /** Multer gives us file info in req.file object */
+            if(!req.file){
+                res.json({error_code:1,err_desc:"No file passed"});
+                return;
+            }
+            /** Check the extension of the incoming file and
+             *  use the appropriate module
+             */
+            if(req.file.originalname.split('.')[req.file.originalname.split('.').length-1] === 'xlsx'){
+                exceltojson = xlsxtojson;
+            } else {
+                exceltojson = xlstojson;
+            }
+            console.log(req.file.path);
+            try {
+                exceltojson({
+                    input: req.file.path,
+                    output: null, //since we don't need output.json
+                    lowerCaseHeaders:true
+                }, function(err,result){
+                    if(err) {
+                        return res.json({error_code:1,err_desc:err, data: null});
+                    }
+                    res.json({error_code:0,err_desc:null, data: result});
+                });
+            } catch (e){
+                res.json({error_code:1,err_desc:"Corupted excel file"});
+            }
+        })
+        /*
+        var exceltojson;
+        upload(req,res,function(err){
           if(err){
-           return;
+            console.log(err);
+            return;
           }
-          /** Multer gives us file info in req.file object */
+          // Multer gives us file info in req.file object
           if(!req.file){
+            console.log("No hay archivo");
+            res.send("No hay archivo")
             return;
           }
 
@@ -455,7 +502,7 @@ router.route("/new/excel")
             res.redirect("/products");
           }
 
-        });
+        });*/
 
       });
 
