b9370bba5678cdce4963965b9e32983433cb05fb
Raul Perez <raulperod@gmail.com>
2017-03-16 16:02:47 -0700

    routes_historial update

--- app_server/routes/routes_historial.js
+++ app_server/routes/routes_historial.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var RegistroDeMovimiento = require("../models/registroDeMovimiento").RegistroDeMovimiento;
 var Baja = require("../models/baja").Baja;
+var Tecnica = require("../models/tecnica").Tecnica;
+var Producto = require("../models/producto").Producto;
+var Sucursal = require("../models/tecnica").Sucursal;
 var router = express.Router();
 // gelishtime/movimientos
 // Metodo GET
@@ -63,11 +66,43 @@ router.get("/bajas",function(req,res){
 router.get("/estadisticas/general",function(req,res){
   // mandar bajas de productos basicos y no basicos
   // primero busco las bajas de los productos basicos
-  Baja.find({tecnica: { "$exists" : true }},{_id:0,sucursal:1,producto:1,tecnica:1,cantidad:1,fecha:1}).populate("sucursal producto tecnica").exec(function(err,basicos){
-    if(!err && basicos){ // si no hubo error y hay bajas
-      Baja.find({tecnica: { "$exists" : false }},{_id:0,sucursal:1,producto:1,cantidad:1,fecha:1}).populate("sucursal producto").exec(function(err,productos){
-        if(!err && productos){ // si no hubo error y hay bajas
-          res.render("./historial/estadisticas/general",{productos:productos,basicos:basicos});
+  Baja.find({tecnica: { "$exists" : true }},{_id:0,sucursal:1,producto:1,tecnica:1,cantidad:1,fecha:1}).populate("sucursal producto tecnica").exec(function(err,bajasBasicos){
+    if(!err && bajasBasicos){ // si no hubo error y hay bajas
+      Baja.find({tecnica: { "$exists" : false }},{_id:0,sucursal:1,producto:1,cantidad:1,fecha:1}).populate("sucursal producto").exec(function(err,bajasProductos){
+        if(!err && bajasProductos){ // si no hubo error y hay bajas
+          // busco todas las tecnicas
+          Tecnica.find({},{_id:0,nombreCompleto:1,sucursal:1}).populate("sucursal").exec(function(err,tecnicas){
+            if(!err && tecnicas){ // si no hubo error y hay tecnicas
+              // busco los productos basicos
+              Producto.find({esBasico:true},{_id:0,nombre:true}).exec(function(err,basicos){
+                if(!err && basicos){ // si no hubo error y hay basicos
+                  // busco los productos no basicos
+                  Producto.find({esBasico:false},{_id:0,nombre:true}).exec(function(err,productos){
+                    if(!err && productos){ // si no hubo error y hay productos
+                      // busco las sucursales
+                      Sucursal.find({},{_id:0,plaza:1}).exec(function(err,sucursales){
+                        if(!err && sucursales){ // si no hubo error y hay sucursales
+                          res.render("./historial/estadisticas/general",{bajasProductos:bajasProductos,bajasBasicos:bajasBasicos,tecnicas:tecnicas,basicos:basicos,productos:productos,sucursales:sucursales});
+                        }else{ // si paso algo
+                          if(err) console.log(err);
+                          res.redirect("/almacen");
+                        }
+                      });
+                    }else{ // si paso algo
+                      if(err) console.log(err);
+                      res.redirect("/almacen");
+                    }
+                  });
+                }else{ // si paso algo
+                  if(err) console.log(err);
+                  res.redirect("/almacen");
+                }
+              });
+            }else{ // si paso un error
+              if(err) console.log(err);
+              res.redirect("/almacen");
+            }
+          });
         }else{ // si hubo error
           if(err) console.log(err);
           res.redirect("/almacen");
@@ -84,11 +119,35 @@ router.get("/estadisticas/general",function(req,res){
 router.get("/estadisticas/sucursal",function(req,res){
   // mandar bajas de productos basicos y no basicos
   // primero busco las bajas de los productos basicos
-  Baja.find({sucursal:res.locals.usuario.sucursal,tecnica: { "$exists" : true }},{_id:0,producto:1,tecnica:1,cantidad:1,fecha:1}).populate("producto tecnica").exec(function(err,basicos){
-    if(!err && basicos){ // si no hubo error y hay bajas
-      Baja.find({sucursal:res.locals.usuario.sucursal,tecnica: { "$exists" : false }},{_id:0,producto:1,cantidad:1,fecha:1}).populate("producto").exec(function(err,productos){
-        if(!err && productos){ // si no hubo error y hay bajas
-          res.render("./historial/estadisticas/general",{productos:productos,basicos:basicos});
+  Baja.find({sucursal:res.locals.usuario.sucursal,tecnica: { "$exists" : true }},{_id:0,producto:1,tecnica:1,cantidad:1,fecha:1}).populate("producto tecnica").exec(function(err,bajasBasicos){
+    if(!err && bajasBasicos){ // si no hubo error y hay bajas
+      Baja.find({sucursal:res.locals.usuario.sucursal,tecnica: { "$exists" : false }},{_id:0,producto:1,cantidad:1,fecha:1}).populate("producto").exec(function(err,bajasProductos){
+        if(!err && bajasProductos){ // si no hubo error y hay bajas
+          // busco todas las tecnicas
+          Tecnica.find({sucursal:res.locals.usuario.sucursal},{_id:0,nombreCompleto:1}).exec(function(err,tecnicas){
+            if(!err && tecnicas){ // si no hubo error y hay tecnicas
+              // busco los productos basicos
+              Producto.find({esBasico:true},{_id:0,nombre:true}).exec(function(err,basicos){
+                if(!err && basicos){ // si no hubo error y hay basicos
+                  // busco los productos no basicos
+                  Producto.find({esBasico:false},{_id:0,nombre:true}).exec(function(err,productos){
+                    if(!err && productos){ // si no hubo error y hay productos
+                      res.render("./historial/estadisticas/sucursal",{bajasProductos:bajasProductos,bajasBasicos:bajasBasicos,tecnicas:tecnicas,basicos:basicos,productos:productos});
+                    }else{ // si paso algo
+                      if(err) console.log(err);
+                      res.redirect("/almacen");
+                    }
+                  });
+                }else{ // si paso algo
+                  if(err) console.log(err);
+                  res.redirect("/almacen");
+                }
+              });
+            }else{ // si paso un error
+              if(err) console.log(err);
+              res.redirect("/almacen");
+            }
+          });
         }else{ // si hubo error
           if(err) console.log(err);
           res.redirect("/almacen");
@@ -99,7 +158,5 @@ router.get("/estadisticas/sucursal",function(req,res){
       res.redirect("/almacen");
     }
   });
-
-
 });
 module.exports = router;
