8ed7286371cab7032808fd3b3960d82342a1795f
Raul Perez <raulperod@gmail.com>
2017-04-13 18:46:20 -0700

    subida de la ruta historial

--- app_server/controllers/historial.js
+++ app_server/controllers/historial.js
@@ -1,3 +1,200 @@
 /**
  * Created by Raul Perez on 13/04/2017.
  */
+'use strict'
+
+const RegistroDeMovimiento = require("../models/registroDeMovimiento"),
+      Baja = require("../models/baja"),
+      Tecnica = require("../models/tecnica"),
+      Producto = require("../models/producto"),
+      Sucursal = require("../models/sucursal")
+
+function historialMovimientosGet(req, res) {
+    let usuario = req.session.user
+    if( usuario.permisos === 1){ // si es administrador de sucursales
+        // busca los movimientos de la sucursal del administrador
+        RegistroDeMovimiento.find({ sucursal: usuario.sucursal })
+            .populate("usuario producto tecnica")
+            .exec( (err, movimientos) => {
+                if(!err && movimientos){ // si no hay error y hay movimientos
+                    res.render("./historial/movimientos",{movimientos, usuario})
+                }else{ // si hubo error
+                    if(err) console.log(err)
+                    res.redirect("/almacen")
+                }
+            })
+    }else{ // si es administrador general
+        // busca los movimientos de todas las sucursales
+        RegistroDeMovimiento.find({})
+            .populate("usuario producto sucursal tecnica")
+            .exec( (err, movimientos) => {
+                if(!err && movimientos){ // si no hubo error y hay movimientos
+                    res.render("./historial/movimientos",{movimientos, usuario })
+                }else{ // si hubo error
+                    if(err) console.log(err)
+                    res.redirect("/almacen")
+                }
+            })
+    }
+}
+
+function historialBajasGet(req, res) {
+    let usuario = req.session.user
+    if( usuario.permisos === 1){ // si es administrador de sucursal
+        // busca las bajas de la sucursal
+        Baja.find({sucursal: usuario.sucursal})
+            .populate("usuario producto tecnica")
+            .exec( (err, bajas) => {
+                if(!err && bajas){ // si no hubo error y hay bajas
+                    res.render("./historial/bajas",{bajas, usuario})
+                }else{ // si hubo un error
+                    if(err) console.log(err)
+                    res.redirect("/almacen")
+                }
+            })
+    }else{ // si es administrador general
+        // busca todas las bajas de las sucursales
+        Baja.find({}).populate("usuario producto sucursal").exec( (err, bajas) => {
+            if(!err && bajas){ // si no hay error y existen bajas
+                res.render("./historial/bajas",{bajas, usuario})
+            }else{ // si hubo un error
+                if(err) console.log(err)
+                res.redirect("/almacen")
+            }
+        })
+    }
+}
+
+function historialGeneralGet(req, res) {
+    Sucursal.find({},{ _id: 0, plaza: 1 }).exec( (err, sucursales) => {
+        if(!err){
+            Producto.find({ esBasico: true }, { _id: 0, nombre: 1 }).exec( (err, basicos) =>  {
+                if(!err){
+                    res.render("./historial/general",{sucursales, basicos, usuario: req.session.user})
+                }else{
+                    console.log(err)
+                    res.redirect("/almacen")
+                }
+            })
+        }else{
+            console.log(err)
+            res.redirect("/almacen")
+        }
+    })
+}
+
+function historialSucursalGet(req, res) {
+    Producto.find({ esBasico: true }, { _id: 0, nombre: 1 }).exec( (err, basicos) => {
+        if(!err){
+            res.render("./historial/sucursal",{basicos, usuario: req.session.user })
+        }else{
+            console.log(err)
+            res.redirect("/almacen")
+        }
+    })
+}
+
+function historialDatosGeneralGet(req, res) {
+    // mandar bajas de productos basicos y no basicos
+    // primero busco las bajas de los productos basicos
+    let datos = {},
+        promesa = new Promise( (resolve, reject) => {
+            Baja.find({ tecnica: { "$exists" : true }}, { _id: 0, sucursal: 1, producto: 1, tecnica: 1, cantidad: 1, fecha: 1 }).populate("sucursal producto tecnica").exec( (err, bajasBasicos) => {
+                return (err) ? reject( new Error('Error') ) : resolve(bajasBasicos)
+            })
+        })
+
+    promesa
+            .then( resolved => {
+                datos.bajasBasicos = resolved
+                return new Promise((resolve, reject) => {
+                    Tecnica.find({}, { _id: 0, nombreCompleto: 1, sucursal: 1 }).populate("sucursal").exec( (err, tecnicas) => {
+                        return (err) ? reject( new Error('Error') ) : resolve(tecnicas)
+                    })
+                })
+            })
+            .then( resolved => {
+                datos.tecnicas = resolved
+                return new Promise((resolve, reject) => {
+                    Producto.find({ esBasico: true }, { _id: 0, nombre: 1 }).exec( (err, basicos) => {
+                        return (err) ? reject( new Error('Error') ) : resolve(basicos)
+                    })
+                })
+            })
+            .then( resolved => {
+                datos.basicos = resolved
+                return new Promise((resolve, reject) => {
+                    Producto.find({ esBasico: false }, { _id: 0, nombre: 1 }).exec( (err, productos) => {
+                        return (err) ? reject( new Error('Error') ) : resolve(productos)
+                    })
+                })
+            })
+            .then( resolved => {
+                datos.productos = resolved
+                return new Promise((resolve, reject) => {
+                    Sucursal.find({},{ _id: 0, plaza: 1 }).exec( (err, sucursales) => {
+                        return (err) ? reject( new Error('Error') ) : resolve(sucursales)
+                    })
+                })
+            })
+            .then( resolved => {
+                datos.sucursales = resolved
+                res.json(datos)
+            })
+            .catch( error => {
+                res.json({error})
+            })
+}
+
+function historialDatosSucursalGet(req, res) {
+    // mandar bajas de productos basicos y no basicos
+    // primero busco las bajas de los productos basicos
+    let datos = {},
+        promesa = new Promise( (resolve, reject) => {
+            Baja.find({ tecnica: { "$exists" : true }}, { _id: 0, sucursal: 1, producto: 1, tecnica: 1, cantidad: 1, fecha: 1 }).populate("sucursal producto tecnica").exec( (err, bajasBasicos) => {
+                return (err) ? reject( new Error('Error') ) : resolve(bajasBasicos)
+            })
+        })
+
+    promesa
+        .then( resolved => {
+            datos.bajasBasicos = resolved
+            return new Promise((resolve, reject) => {
+                Tecnica.find({}, { _id: 0, nombreCompleto: 1, sucursal: 1 }).populate("sucursal").exec( (err, tecnicas) => {
+                    return (err) ? reject( new Error('Error') ) : resolve(tecnicas)
+                })
+            })
+        })
+        .then( resolved => {
+            datos.tecnicas = resolved
+            return new Promise((resolve, reject) => {
+                Producto.find({ esBasico: true }, { _id: 0, nombre: 1 }).exec( (err, basicos) => {
+                    return (err) ? reject( new Error('Error') ) : resolve(basicos)
+                })
+            })
+        })
+        .then( resolved => {
+            datos.basicos = resolved
+            return new Promise((resolve, reject) => {
+                Producto.find({ esBasico: false }, { _id: 0, nombre: 1 }).exec( (err, productos) => {
+                    return (err) ? reject( new Error('Error') ) : resolve(productos)
+                })
+            })
+        })
+        .then( resolved => {
+            datos.productos = resolved
+            res.json(datos)
+        })
+        .catch( error => {
+            res.json({error})
+        })
+}
+
+module.exports = {
+    historialMovimientosGet,
+    historialBajasGet,
+    historialGeneralGet,
+    historialSucursalGet,
+    historialDatosGeneralGet,
+    historialDatosSucursalGet
+}
--- app_server/routes/routes_historial.js
+++ app_server/routes/routes_historial.js
@@ -1,193 +1,26 @@
-var express = require("express");
-var RegistroDeMovimiento = require("../models/registroDeMovimiento").RegistroDeMovimiento;
-var Baja = require("../models/baja").Baja;
-var Tecnica = require("../models/tecnica").Tecnica;
-var Producto = require("../models/producto").Producto;
-var Sucursal = require("../models/sucursal").Sucursal;
-var router = express.Router();
-// gelishtime/movimientos
+'use strict'
+
+const express = require("express"),
+      HistorialController = require('../controllers/historial'),
+      historial = express.Router()
+
+// gelishtime/historial/movimientos
 // Metodo GET
-router.get("/movimientos",function(req,res){
-  if(res.locals.usuario.permisos == 1){ // si es administrador de sucursales
-    // busca los movimientos de la sucursal del administrador
-    RegistroDeMovimiento.find({sucursal:res.locals.usuario.sucursal})
-                        .populate("usuario producto tecnica")
-                        .exec(function(err,movimientos){
-      if(!err && movimientos){ // si no hay error y hay movimientos
-        res.render("./historial/movimientos",{movimientos:movimientos});
-      }else{ // si hubo error
-        if(err) console.log(err);
-        res.redirect("/almacen");
-      }
-    });
-  }else{ // si es administrador general
-    // busca los movimientos de todas las sucursales
-    RegistroDeMovimiento.find({})
-                        .populate("usuario producto sucursal tecnica")
-                        .exec(function(err,movimientos){
-      if(!err && movimientos){ // si no hubo error y hay movimientos
-        res.render("./historial/movimientos",{movimientos:movimientos});
-      }else{ // si hubo error
-        if(err) console.log(err);
-        res.redirect("/almacen");
-      }
-    });
-  }
-});
-// gelishtime/bajas
+historial.get("/movimientos", HistorialController.historialMovimientosGet)
+// gelishtime/historial/bajas
 // Metodo GET
-router.get("/bajas",function(req,res){
-  if(res.locals.usuario.permisos == 1){ // si es administrador de sucursal
-    // busca las bajas de la sucursal
-    Baja.find({sucursal:res.locals.usuario.sucursal})
-        .populate("usuario producto tecnica")
-        .exec(function(err,bajas){
-      if(!err && bajas){ // si no hubo error y hay bajas
-        res.render("./historial/bajas",{bajas:bajas});
-      }else{ // si hubo un error
-        if(err) console.log(err);
-        res.redirect("/almacen");
-      }
-    });
-  }else{ // si es administrador general
-    // busca todas las bajas de las sucursales
-    Baja.find({}).populate("usuario producto sucursal").exec(function(err,bajas){
-      if(!err && bajas){ // si no hay error y existen bajas
-        res.render("./historial/bajas",{bajas:bajas});
-      }else{ // si hubo un error
-        if(err) console.log(err);
-        res.redirect("/almacen");
-      }
-    });
-  }
-});
-// gelishtime/general
+historial.get("/bajas", HistorialController.historialBajasGet )
+// gelishtime/historial/general
 // Metodo GET
-router.get("/general",function(req,res){
-  Sucursal.find({},{_id:0,plaza:1}).exec(function(err,sucursales){
-    if(!err){
-      Producto.find({esBasico:true},{_id:0,nombre:1}).exec(function(err,basicos){
-        if(!err){
-          res.render("./historial/general",{sucursales:sucursales,basicos:basicos});
-        }else{
-          console.log(err);
-          res.redirect("/almacen");
-        }
-      });
-    }else{
-      console.log(err);
-      res.redirect("/almacen");
-    }
-  });
-});
-// gelishtime/sucursal
+historial.get("/general", HistorialController.historialGeneralGet )
+// gelishtime/historial/sucursal
 // Metodo GET
-router.get("/sucursal",function(req,res){
-  Producto.find({esBasico:true},{_id:0,nombre:1}).exec(function(err,basicos){
-    if(!err){
-      res.render("./historial/sucursal",{basicos:basicos});
-    }else{
-      console.log(err);
-      res.redirect("/almacen");
-    }
-  });
-});
-// gelishtime/datosGeneral
+historial.get("/sucursal", HistorialController.historialSucursalGet )
+// gelishtime/historial/datosGeneral
 // Metodo GET
-router.get("/datosGeneral",function(req,res){
-  // mandar bajas de productos basicos y no basicos
-  // primero busco las bajas de los productos basicos
-  Baja.find({tecnica: { "$exists" : true }},{_id:0,sucursal:1,producto:1,tecnica:1,cantidad:1,fecha:1}).populate("sucursal producto tecnica").exec(function(err,bajasBasicos){
-    if(!err && bajasBasicos){ // si no hubo error y hay bajas
-      Baja.find({tecnica: { "$exists" : false }},{_id:0,sucursal:1,producto:1,cantidad:1,fecha:1}).populate("sucursal producto").exec(function(err,bajasProductos){
-        if(!err && bajasProductos){ // si no hubo error y hay bajas
-          // busco todas las tecnicas
-          Tecnica.find({},{_id:0,nombreCompleto:1,sucursal:1}).populate("sucursal").exec(function(err,tecnicas){
-            if(!err && tecnicas){ // si no hubo error y hay tecnicas
-              // busco los productos basicos
-              Producto.find({esBasico:true},{_id:0,nombre:1}).exec(function(err,basicos){
-                if(!err && basicos){ // si no hubo error y hay basicos
-                  // busco los productos no basicos
-                  Producto.find({esBasico:false},{_id:0,nombre:1}).exec(function(err,productos){
-                    if(!err && productos){ // si no hubo error y hay productos
-                      // busco las sucursales
-                      Sucursal.find({},{_id:0,plaza:1}).exec(function(err,sucursales){
-                        if(!err && sucursales){ // si no hubo error y hay sucursales
-                          res.json({bajasProductos:bajasProductos,bajasBasicos:bajasBasicos,tecnicas:tecnicas,basicos:basicos,productos:productos,sucursales:sucursales});
-                        }else{ // si paso algo
-                          if(err) console.log(err);
-                          res.redirect("/almacen");
-                        }
-                      });
-                    }else{ // si paso algo
-                      if(err) console.log(err);
-                      res.redirect("/almacen");
-                    }
-                  });
-                }else{ // si paso algo
-                  if(err) console.log(err);
-                  res.redirect("/almacen");
-                }
-              });
-            }else{ // si paso un error
-              if(err) console.log(err);
-              res.redirect("/almacen");
-            }
-          });
-        }else{ // si hubo error
-          if(err) console.log(err);
-          res.redirect("/almacen");
-        }
-      });
-    }else{ // si hubo error
-      if(err) console.log(err);
-      res.redirect("/almacen");
-    }
-  });
-});
-// gelishtime/datosSucursal
+historial.get("/datosGeneral", HistorialController.historialDatosGeneralGet )
+// gelishtime/historial/datosSucursal
 // Metodo GET
-router.get("/datosSucursal",function(req,res){
-  // mandar bajas de productos basicos y no basicos
-  // primero busco las bajas de los productos basicos
-  Baja.find({sucursal:res.locals.usuario.sucursal,tecnica: { "$exists" : true }},{_id:0,producto:1,tecnica:1,cantidad:1,fecha:1}).populate("producto tecnica").exec(function(err,bajasBasicos){
-    if(!err && bajasBasicos){ // si no hubo error y hay bajas
-      Baja.find({sucursal:res.locals.usuario.sucursal,tecnica: { "$exists" : false }},{_id:0,producto:1,cantidad:1,fecha:1}).populate("producto").exec(function(err,bajasProductos){
-        if(!err && bajasProductos){ // si no hubo error y hay bajas
-          // busco todas las tecnicas
-          Tecnica.find({sucursal:res.locals.usuario.sucursal},{_id:0,nombreCompleto:1}).exec(function(err,tecnicas){
-            if(!err && tecnicas){ // si no hubo error y hay tecnicas
-              // busco los productos basicos
-              Producto.find({esBasico:true},{_id:0,nombre:1}).exec(function(err,basicos){
-                if(!err && basicos){ // si no hubo error y hay basicos
-                  // busco los productos no basicos
-                  Producto.find({esBasico:false},{_id:0,nombre:1}).exec(function(err,productos){
-                    if(!err && productos){ // si no hubo error y hay productos
-                      res.json({bajasProductos:bajasProductos,bajasBasicos:bajasBasicos,tecnicas:tecnicas,basicos:basicos,productos:productos});
-                    }else{ // si paso algo
-                      if(err) console.log(err);
-                      res.redirect("/almacen");
-                    }
-                  });
-                }else{ // si paso algo
-                  if(err) console.log(err);
-                  res.redirect("/almacen");
-                }
-              });
-            }else{ // si paso un error
-              if(err) console.log(err);
-              res.redirect("/almacen");
-            }
-          });
-        }else{ // si hubo error
-          if(err) console.log(err);
-          res.redirect("/almacen");
-        }
-      });
-    }else{ // si hubo error
-      if(err) console.log(err);
-      res.redirect("/almacen");
-    }
-  });
-});
-module.exports = router;
+historial.get("/datosSucursal", HistorialController.historialDatosSucursalGet )
+
+module.exports = historial;
