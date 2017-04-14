d3472cfcaaec698eb8b02bf83c802772552d9676
Raul Perez <raulperod@gmail.com>
2017-04-14 11:45:45 -0700

    correciones

--- app_server/models/config.js
+++ app_server/models/config.js
@@ -4,7 +4,7 @@
 'use strict'
 
 module.exports = {
-    //MONGODB_URL : 'mongodb://admin:gelishtime2017@ds139480.mlab.com:39480/heroku_z357mht1'
+    MONGODB_URL : 'mongodb://admin:gelishtime2017@ds139480.mlab.com:39480/heroku_z357mht1'
     //MONGODB_URL : process.env.MONGODB_URI
-    MONGODB_URL : "mongodb://localhost/gelishtime"
+    //MONGODB_URL : "mongodb://localhost/gelishtime"
 }
