075c86cfcf114806baf7947998988313fcdb9cf8
Raul Perez <raulperod@gmail.com>
2017-04-13 17:36:23 -0700

    segunda subida. lista la ruta categories

--- app_server/controllers/categoria.js
+++ app_server/controllers/categoria.js
@@ -54,15 +54,91 @@ function categoriesNewPost(req, res) {
 }
 
 function categoriesIdCategoryGet(req, res) {
-
+    // busco la categoria
+    Categoria.findById(req.params.idCategoria).exec( (err, categoryUpdate) => {
+        if(!err && categoryUpdate){// si no hubo error y la categoria existe
+            res.render("./categories/update",{ usuario: req.session.user, categoryUpdate, AlertNombre:false})
+        }else{
+            // imprimo el error y lo redirecciono al administrador de sucursales
+            if(err) console.log(err)
+            res.redirect("/categories")
+        }
+    })
 }
 
 function categoriesIdCategoryPut(req, res) {
-
+    // busco la categoria
+    Categoria.findById(req.params.idCategoria).exec( (err,categoria) => {
+        if(!err && categoria){// si no hay error y la categoria existe
+            if(categoria.nombre === req.body.nombre){ // si no modifico su nombre
+                res.locals.categoryUpdate = categoria
+                res.locals.categoryUpdate.descripcion = req.body.descripcion
+                res.locals.categoryUpdate.save( err => {
+                    if(err) console.log(err)
+                    res.redirect("/categories")
+                })
+            }else{ // si modifico su nombre
+                // verifica que el nuevo nombre no este repetido
+                Categoria.findOne({nombre:req.body.nombre}).exec( (err, categoriaNew) => {
+                    if(!err && !categoriaNew){
+                        // si no se repite entonces actualizo la categoria
+                        res.locals.categoryUpdate = categoria
+                        res.locals.categoryUpdate.nombre = req.body.nombre
+                        res.locals.categoryUpdate.descripcion = req.body.descripcion
+                        res.locals.categoryUpdate.save( err => {
+                            if(err) console.log(err)
+                            res.redirect("/categories")
+                        })
+                    }else{ // si paso algo
+                        if(categoriaNew){ // si hay una categoria con el nuevo nombre
+                            // mando una alerta
+                            res.render("./categories/update",{ usuario: req.session.user, AlertNombre: true, nombre: req.body.nombre, descripcion: req.body.descripcion, id: req.params.idCategoria})
+                        }else{ // si hay error
+                            console.log(err)
+                            res.redirect("/categoria")
+                        }
+                    }
+                })
+            }
+        }else{ // si paso algo
+            if(err) console.log(err)
+            res.redirect("/categories")
+        }
+    })
 }
 
 function categoriesIdCategoryDelete(req, res) {
-
+    // busco todos los productos que tengan la catedoria
+    Producto.find({categoria:req.params.idCategoria}).exec( (err, productos) => {
+        if(!err && productos){
+            // borro todos los productos de la categoria a eliminar
+            for(let producto of productos){
+                Baja.remove({producto: producto._id}).exec( err => {
+                    if(err) console.log(err)
+                })
+                RegistroDeMovimiento.remove({producto:producto._id}).exec( err => {
+                    if(err) console.log(err)
+                })
+                Consumo.remove({producto:producto._id}).exec( err => {
+                    if(err) console.log(err)
+                })
+                Almacen.remove({producto:producto._id}).exec( err => {
+                    if(err) console.log(err)
+                })
+                Producto.findOneAndRemove({_id:producto._id}).exec( err => {
+                    if(err) console.log(err)
+                })
+            }
+        }else{ // si paso un error
+            if(err) console.log(err) // imprimo el error
+            res.redirect("/categories")
+        }
+    })
+    // por ultimo borro la categoria
+    Categoria.findOneAndRemove({_id: req.params.idCategoria}).exec( err => {
+        if(err) console.log(err)
+        res.redirect("/categories")
+    })
 }
 
 module.exports = {
--- app_server/routes/routes_category.js
+++ app_server/routes/routes_category.js
@@ -16,91 +16,9 @@ category
 // gelishtime/categories/:idCategoria
 category
         .route("/:idCategoria")
-        .get(function(req,res){
-          // busco la categoria
-          Categoria.findById(req.params.idCategoria).exec(function(err,categoria){
-            if(!err && categoria){// si no hubo error y la categoria existe
-              res.render("./categories/update",{categoryUpdate:categoria,AlertNombre:false});
-            }else{
-              // imprimo el error y lo redirecciono al administrador de sucursales
-              if(err) console.log(err);
-              res.redirect("/categories");
-            }
-          });
-        })
-        .put(function(req,res){
-          // busco la categoria
-          Categoria.findById(req.params.idCategoria).exec(function(err,categoria){
-            if(!err && categoria){// si no hay error y la categoria existe
-              if(categoria.nombre == req.body.nombre){ // si no modifico su nombre
-                res.locals.categoryUpdate = categoria;
-                res.locals.categoryUpdate.descripcion = req.body.descripcion;
-                res.locals.categoryUpdate.save(function(err){
-                  if(err) console.log(err);
-                  res.redirect("/categories");
-                });
-              }else{ // si modifico su nombre
-                // verifica que el nuevo nombre no este repetido
-                Categoria.findOne({nombre:req.body.nombre}).exec(function(err,categoriaNew){
-                  if(!err && !categoriaNew){
-                    // si no se repite entonces actualizo la categoria
-                    res.locals.categoryUpdate = categoria;
-                    res.locals.categoryUpdate.nombre = req.body.nombre;
-                    res.locals.categoryUpdate.descripcion = req.body.descripcion;
-                    res.locals.categoryUpdate.save(function(err){
-                      if(err) console.log(err);
-                      res.redirect("/categories");
-                    });
-                  }else{ // si paso algo
-                    if(categoriaNew){ // si hay una categoria con el nuevo nombre
-                      // mando una alerta
-                      res.render("./categories/update",{AlertNombre:true,nombre:req.body.nombre,descripcion:req.body.descripcion,id:req.params.idCategoria});
-                    }else{ // si hay error
-                      console.log(err);
-                      res.redirect("/categoria");
-                    }
-                  }
-                });
-              }
-            }else{ // si paso algo
-              if(err) console.log(err);
-              res.redirect("/categories");
-            }
-          });
-        })
+        .get( CategoryController.categoriesIdCategoryGet )
+        .put( CategoryController.categoriesIdCategoryPut )
         // Metodo DELETE
-        .delete(function(req,res){
-        // busco todos los productos que tengan la catedoria
-        Producto.find({categoria:req.params.idCategoria}).exec(function(err,productos){
-            if(!err && productos){
-              // borro todos los productos de la categoria a eliminar
-              for(let producto of productos){
-                Baja.remove({producto:producto._id}).exec(function(err){
-                  if(err) console.log(err);
-                });
-                RegistroDeMovimiento.remove({producto:producto._id}).exec(function(err){
-                  if(err) console.log(err);
-                });
-                Consumo.remove({producto:producto._id}).exec(function(err){
-                  if(err) console.log(err);
-                });
-                Almacen.remove({producto:producto._id}).exec(function(err){
-                  if(err) console.log(err);
-                });
-                Producto.findOneAndRemove({_id:producto._id}).exec(function(err){
-                    if(err) console.log(err);
-                });
-              }
-            }else{ // si paso un error
-              if(err) console.log(err); // imprimo el error
-              res.redirect("/categories");
-            }
-        });
-        // por ultimo borro la categoria
-        Categoria.findOneAndRemove({_id: req.params.idCategoria}).exec(function(err){
-            if(err) console.log(err);
-            res.redirect("/categories");
-        });
-  })
+        .delete( CategoryController.categoriesIdCategoryDelete )
 
 module.exports = category
